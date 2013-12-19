7de3a75644571cf66b56065ce66dd83ba13d5a3e
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-12-19 22:01:08 +0100

    Added logout function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var optionsProducer = require('./lib/options-producer');
 var loginChecker = require('./lib/login-checker');
 var logoutEventReceiver = require('./lib/logout-event-receiver');
-
+var logout = require('./lib/logout');
 
 module.exports = function(app, options) {
   options = optionsProducer(options);
 
   app.use(loginChecker(options));
   app.use(logoutEventReceiver(options));
+  app.post('/logout', logout(options));
 };
